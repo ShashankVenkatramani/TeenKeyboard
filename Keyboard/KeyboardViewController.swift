90e517a8150e4a210f4983bc1602b37ca4f35085
Shashank <shashankvxp@gmail.com>
2018-04-15 05:30:43 -0700

    working shit

--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -8,124 +8,127 @@
 
 import UIKit
 import CoreML
+
+ var proxy : UITextDocumentProxy?
+
 class KeyboardViewController: UIInputViewController {
     var totalText:String? = ""
     
     private let model = SentimentPolarity()
     
     
-    @IBOutlet weak var nextKeyboardButton: UIButton!
+    @IBOutlet var nextKeyboardButton: UIButton?
     
     @IBAction func q(_ sender: Any) {
-        proxy.insertText("q")
+        proxy?.insertText("q")
         totalText = totalText! + "q"
     }
     @IBAction func w(_ sender: Any) {
-        proxy.insertText("w")
+        proxy?.insertText("w")
         totalText = totalText! + "w"
     }
     @IBAction func e(_ sender: Any) {
-        proxy.insertText("e")
+        proxy?.insertText("e")
         totalText = totalText! + "e"
     }
     @IBAction func r(_ sender: Any) {
-        proxy.insertText("r")
+        proxy?.insertText("r")
         totalText = totalText! + "r"
     }
     @IBAction func t(_ sender: Any) {
-        proxy.insertText("t")
+        proxy?.insertText("t")
         totalText = totalText! + "t"
     }
     @IBAction func y(_ sender: Any) {
-        proxy.insertText("y")
+        proxy?.insertText("y")
         totalText = totalText! + "y"
     }
     @IBAction func u(_ sender: Any) {
-        proxy.insertText("u")
+        proxy?.insertText("u")
         totalText = totalText! + "u"
     }
     @IBAction func i(_ sender: Any) {
-        proxy.insertText("i")
+        proxy?.insertText("i")
         totalText = totalText! + "i"
     }
     @IBAction func o(_ sender: Any) {
-        proxy.insertText("o")
+        proxy?.insertText("o")
         totalText = totalText! + "o"
     }
     @IBAction func p(_ sender: Any) {
-        proxy.insertText("p")
+        proxy?.insertText("p")
         totalText = totalText! + "p"
     }
     @IBAction func a(_ sender: Any) {
-        proxy.insertText("a")
+        proxy?.insertText("a")
         totalText = totalText! + "a"
     }
     @IBAction func s(_ sender: Any) {
-        proxy.insertText("s")
+        proxy?.insertText("s")
         totalText = totalText! + "s"
     }
     @IBAction func d(_ sender: Any) {
-        proxy.insertText("d")
+        proxy?.insertText("d")
         totalText = totalText! + "d"
     }
     @IBAction func f(_ sender: Any) {
-        proxy.insertText("f")
+        proxy?.insertText("f")
         totalText = totalText! + "f"
     }
     @IBAction func g(_ sender: Any) {
-        proxy.insertText("g")
+        proxy?.insertText("g")
         totalText = totalText! + "g"
     }
     @IBAction func h(_ sender: Any) {
-        proxy.insertText("h")
+        proxy?.insertText("h")
         totalText = totalText! + "h"
     }
     @IBAction func j(_ sender: Any) {
-        proxy.insertText("j")
+        proxy?.insertText("j")
         totalText = totalText! + "j"
     }
     @IBAction func k(_ sender: Any) {
-        proxy.insertText("k")
+        proxy?.insertText("k")
         totalText = totalText! + "k"
     }
     @IBAction func l(_ sender: Any) {
-        proxy.insertText("l")
+        proxy?.insertText("l")
         totalText = totalText! + "l"
     }
     @IBAction func shift(_ sender: Any) {
-        proxy.insertText("shift")
+        proxy?.insertText("shift")
         totalText = totalText! + ""
     }
     @IBAction func z(_ sender: Any) {
-        proxy.insertText("z")
+        proxy?.insertText("z")
         totalText = totalText! + "z"
     }
     @IBAction func x(_ sender: Any) {
-        proxy.insertText("x")
+        proxy?.insertText("x")
         totalText = totalText! + "x"
     }
     @IBAction func c(_ sender: Any) {
-        proxy.insertText("c")
+        proxy?.insertText("c")
         totalText = totalText! + "c"
     }
     @IBAction func v(_ sender: Any) {
-        proxy.insertText("v")
+        proxy?.insertText("v")
         totalText = totalText! + "v"
     }
     @IBAction func b(_ sender: Any) {
-        proxy.insertText("b")
+        proxy?.insertText("b")
         totalText = totalText! + "b"
     }
     @IBAction func n(_ sender: Any) {
-        proxy.insertText("n")
+        proxy?.insertText("n")
         totalText = totalText! + "n"
     }
     @IBAction func m(_ sender: Any) {
-        proxy.insertText("m")
+        proxy?.insertText("m")
         totalText = totalText! + "n"
     }
     @IBAction func del(_ sender: Any) {
-        proxy.deleteBackward()
+        proxy?.deleteBackward()
         
         totalText = String((totalText?.characters.dropLast())!)
         
@@ -133,7 +136,7 @@ class KeyboardViewController: UIInputViewController {
         
     }
     @IBAction func space(_ sender: Any) {
-        proxy.insertText(" ")
+        proxy?.insertText(" ")
         totalText = totalText! + " "
     }
     @IBAction func returnKey(_ sender: Any) {
@@ -141,13 +144,13 @@ class KeyboardViewController: UIInputViewController {
         print(totalText)
         
         if(makePrediction(textUse: totalText!) == true){
-            proxy.insertText("\n")
+            proxy?.insertText("\n")
             totalText = ""
             print("positive")
         } else {
             
             for char in totalText! {
-                proxy.deleteBackward()
+                proxy?.deleteBackward()
             }
             
             totalText = ""
@@ -203,13 +206,13 @@ class KeyboardViewController: UIInputViewController {
         
     }
     
-    var proxy : UITextDocumentProxy!
+   
     
     var keyboardView: UIView!
     
-    func loadInterface(){
-        let keyboardNib = UINib(nibName: "KeyboardView", bundle: nil)
-        keyboardView = keyboardNib.instantiate(withOwner: self, options: nil)        [0] as! UIView
+    func loadInterface() {
+        let keyboardNIB = UINib(nibName: "KeyboardView", bundle: nil)
+        keyboardView = keyboardNIB.instantiate(withOwner: self, options: nil) [0] as! UIView
         keyboardView.frame.size = view.frame.size
         view.addSubview(keyboardView)
     }
@@ -223,9 +226,13 @@ class KeyboardViewController: UIInputViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        proxy = textDocumentProxy as UITextDocumentProxy
+        
+        proxy = textDocumentProxy as UITextDocumentProxy?
+       
+        nextKeyboardButton?.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
+        
         loadInterface()
-        nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
+        
         
         
         // Perform custom UI setup here
@@ -262,7 +269,7 @@ class KeyboardViewController: UIInputViewController {
         } else {
             textColor = UIColor.black
         }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+        self.nextKeyboardButton?.setTitleColor(textColor, for: [])
     }
 
 }
